4a1b68c602768bfddca15349c1e056499e5fbf1f
Anderson Dias <andersoninfonet@gmail.com>
2022-02-12 22:11:06 -0300

    begin the project

---
+++ app-security-client/src/test/java/com/gmail/andersoninfonet/appsecurityclient/AppSecurityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.andersoninfonet.appsecurityclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
