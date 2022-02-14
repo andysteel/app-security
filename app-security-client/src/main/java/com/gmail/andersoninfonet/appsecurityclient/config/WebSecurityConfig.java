a2a63c66ff27193b30b902cda9680c3b5d411ec3
Anderson Dias <andersoninfonet@gmail.com>
2022-02-14 00:43:25 -0300

    configuration to connect to auth-server

--- app-security-client/src/main/java/com/gmail/andersoninfonet/appsecurityclient/config/WebSecurityConfig.java
+++ app-security-client/src/main/java/com/gmail/andersoninfonet/appsecurityclient/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.gmail.andersoninfonet.appsecurityclient.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,7 +24,11 @@ public class WebSecurityConfig {
             .csrf()
             .disable()
             .authorizeHttpRequests()
-            .antMatchers("/register").permitAll();
+            .antMatchers("/register", "/verifyRegistration", "/resendVerifyToken").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .and()
+            .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/api-client-oidc"))
+            .oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 }
